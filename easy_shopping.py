a3197278c9ecb6890dbeb5f0d3f50c2619603a09
jsr512 <50751736+jsr512@users.noreply.github.com>
2019-06-28 13:48:31 -0400

    Add dotenv and increase recipe list

--- easy_shopping.py
+++ easy_shopping.py
@@ -1,8 +1,11 @@
 import random
 
 import os
+from dotenv import load_dotenv
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+
+load_dotenv()
 
 ## Input Capture
 valid_entries = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -49,12 +52,20 @@ print(fish_total)
 print(pork_total)
 print(other_total)
 
-## Recipe Data  order must be meat/produce/dairy/spices/other
+## Recipe Data  #must contain meat/produce/dairy/spices/other lists within ingrediants to iterate through with for loop below even if empty
 
 chicken = [
         {"name": "Beer Can Chicken",
         "website": "https://www.foodnetwork.com/recipes/patrick-and-gina-neely/pats-beer-can-grilled-chicken-recipe-1945080",
-        "ingredients": {"meat": ["4-lb whole chicken"], "produce": [], "dairy": [], "spices": [], "other": ["1 12 ounce beer"]}}
+        "ingredients": {"meat": ["4-lb whole chicken"], "produce": [], "dairy": [], "spices": [], "other": ["1 12 ounce beer"]}},
+
+        {"name": "Baked Chicken Parm",
+        "website": "https://www.skinnytaste.com/baked-chicken-parmesan/",
+        "ingredients": {"meat": ["4 8oz chciken breasts"], "produce": [], "dairy": ["1/4 cup grated parmesan", "2 tablespoons butter", "3/4 cup mozzarella"], "spices": [], "other": ["1 cup marinara", "3/4 cup breadcrumbs"]}},
+
+        {"name": "Grilled Chicken Paillard",
+        "website": "https://www.rachaelrayshow.com/recipes/21362_katie_lee_grilled_chicken_paillard_with_arugula_and_shaved_pecorino",
+        "ingredients": {"meat": ["4 boneless chicken breast halves"], "produce": ["grated zest and juice of 1/2 lemon", "8 cups arugula"], "dairy": ["shaved pecorino romano cheese"], "spices": ["1/4 tsp garilic powder", "1/2 tsp grainy mustard"], "other": []}}
 ]
 
 beef = [
@@ -64,7 +75,15 @@ beef = [
         
         {"name": "Cheeseburger Casserole",
         "website": "https://lowcarbyum.com/bacon-cheeseburger-casserole/",
-        "ingredients": {"meat": ["2-lbs ground beef", "1-lb bacon"], "produce": [], "dairy": ["8 eggs", "1 cup heavy cream", "12 ounces grated cheddar"], "spices": ["2 cloves garlic", "1/2 tsp onion powder"], "other": ["1 can tomato paste"]}}
+        "ingredients": {"meat": ["2-lbs ground beef", "1-lb bacon"], "produce": [], "dairy": ["8 eggs", "1 cup heavy cream", "12 ounces grated cheddar"], "spices": ["2 cloves garlic", "1/2 tsp onion powder"], "other": ["1 can tomato paste"]}},
+
+        {"name": "Grilled Steak",
+        "website": "none",
+        "ingrediants": {"meat": ["steak cut of choice"], "produce": [], "dairy": [], "spices": [], "other": []}},
+
+        {"name": "Slow Cooker Broccoli Beef",
+        "website": "https://www.lecremedelacrumb.com/slow-cooker-broccoli-beef/",
+        "ingredients": {"meat": ["1.5-lbs flank steak"], "produce": ["4 cups broccoli"], "dairy": [], "spices": ["1 T garlic", "1/4 tsp red chili flakes"], "other": ["1 cup beef broth", "2/3 cup soy sauce", "1/3 cup brown sugar", "1 T sesame oil", "2 T corn starch"]}}
 ]
 
 fish = [
@@ -72,6 +91,29 @@ fish = [
         "website": "https://www.tasteofhome.com/recipes/baked-lemon-haddock/",
         "ingredients": {"meat": ["2-lb haddock"], "produce": [],"dairy": ["1/4 cup butter"], "spices": ["2 T parsley", "1/2 tsp garlic powder"], "produce": ["2 T Lemon Zest"], "other": ["1 cup bread crumbs"]}}
 ]
+
+pork = [
+        {"name": "Slow Cooker Sausage and Peppers",
+        "website": "https://vanillaandbean.com/lemon-garlic-orzo-with-roasted-vegetables/",
+        "ingredients": {"meat": ["8 italian sausage links"], "produce": ["1 large geen bell pepper", "1 large yellow onion"], "dairy": [], "spices": ["1 tsp fennel seeds", "1/2 tsp oregano", "1/2 tsp basil", "1/2 tsp garlic powder", "1/4 tsp red pepper"], "other": ["28 oz can crushed tomatos", "1/4 cup marsala wine or chicken broth"]}}
+
+]
+
+other = [
+        {"name": "Lemon Garlic Orzo with Roasted Vegtables",
+        "website": "https://foodfolksandfun.net/sausage-peppers-slow-cooker-recipe/",
+        "ingredients": {"meat": [], "produce": ["1 cup mushrooms", "1 cup peppers", "1-lb asparagus", "12 oz cherry tomatos", "1/2 cup shallots", "1 lemon"], "dairy": ["1/2 cup feta"], "spices": ["2 tsp garlic"], "other": ["1 cup orzo", "1.5 cup vegtable broth"]}},
+
+         {"name": "Low Carb Carbonara",
+        "website": "https://sweetcsdesigns.com/paleo-low-carb-carbonara/",
+        "ingredients": {"meat": ["4 slices bacon"], "produce": ["3 zucchinis"], "dairy": ["1 T heavy cream", "2 eggs", "1/3 cup shredded parmesan"], "spices": ["3 cloves garlic"], "other": []}},
+
+         {"name": "Zucchini Rice with Cranberries, Bacon, Goat Cheese, and Walnuts",
+        "website": "https://inspiralized.com/zucchini-rice-with-cranberries-bacon-goat-cheese-and-walnuts-with-maple-dijon-dressing/",
+        "ingredients": {"meat": ["4 slices bacon"], "produce": ["1 zucchini", "1/4 cup dried cranberries"],"dairy": ["1 oz goat cheese"], "spices": [], "other": ["1/3 cup walnuts", "1/5 T apple cider vinegar", "3/4 tsp dijon", "1.5 T maple syrup"]}}
+]
+
+
 
 ## Compiling Shopping List
 
@@ -121,6 +163,7 @@ all_dairy = []
 all_spices = []
 all_other = [] 
 
+
 for item in shopping_list:
     meat_by_meal = (item["meat"])
     all_meat = all_meat + meat_by_meal
@@ -134,8 +177,6 @@ for item in shopping_list:
     all_spices = all_spices + spices_by_meal
 
 compiled_list = all_produce + all_meat + all_dairy + all_spices + all_other
-
-#needs to be string /, loop through list and assemble
 
 final_list = ""
 
